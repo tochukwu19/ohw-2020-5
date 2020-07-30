79d2404d4f9c5f9ec7f3ff53255459e63ae27d55
Tunwase Ayobami <tunwaseayobami98@gmail.com>
2020-07-30 23:18:16 +0100

    Added more webpages and updated image links

--- confirmation/confirmation.js
+++ confirmation/confirmation.js
@@ -4,11 +4,11 @@ const hamburgerSection = document.getElementById('hamburger-section');
 hamburger.addEventListener('click', displayMenu);
 
 function displayMenu(e) {
-    if (e.target.src.includes('id=1os-XVZj9O17KJ4BdoKrG4qAhKn6t7Kvh')) {
+    if (e.target.src.includes('https://res.cloudinary.com/dizrb3vql/image/upload/v1596145703/Group_twsno9.png')) {
         e.target.src = 'https://res.cloudinary.com/dizrb3vql/image/upload/v1596126035/close_pxrklj.svg';
         hamburgerSection.style.display = 'flex';
     } else {
-        e.target.src = 'https://drive.google.com/uc?export=view&id=1os-XVZj9O17KJ4BdoKrG4qAhKn6t7Kvh';
+        e.target.src = 'https://res.cloudinary.com/dizrb3vql/image/upload/v1596145703/Group_twsno9.png';
         hamburgerSection.style.display = 'none';
     }
 }
